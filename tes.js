bfc6e9c5e84d687bb36a9df2c2b092e86c9fad1a
ppgheshan <110448800+ppgheshan@users.noreply.github.com>
2023-05-07 22:19:11 +0530

    simple calculator

---
+++ tes.js
@@ -0,0 +1,22 @@
+function calc(){
+    var num1=parseInt(document.querySelector('#num1').value);
+    var num2=parseInt(document.querySelector('#num2').value);
+    var op=document.querySelector('#op').value;
+    var calculate;
+    if(op=='add'){
+        calculate=num1+num2;
+    }else if(op=='sub'){
+        calculate=num1-num2;
+    }else if(op=='mul'){
+        calculate=num1*num2;
+    }else if(op=='div'){
+        calculate=num1/num2;
+    }
+    
+    alert("Answer is "+calculate);
+}
+function test(){
+    var name=document.querySelector('#name').value;
+    
+    alert("hello "+name);
+}
